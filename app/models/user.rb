18d1f867818575a5bf92d56aedf7fab36846250d
takahashi <icasenyou2018@gmail.com>
2018-12-17 06:29:30 +0000

    SEED追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   has_secure_password
   
   validates :name, {presence: true}
-  validates :email, {presence: true, uniqueness: true}
-  
+  # validates :email, {presence: true, uniqueness: true}
+  validates :email, presence: true, length: { maximum: 255 },#メアドの長さ
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },#メアドが正規であるか
+                    uniqueness: { case_sensitive: false }#重複防止
   
   def reviews
     return Review.where(user_id: self.id)
