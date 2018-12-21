2e013bd3779e719606352d9758b4d96c63184e81
EC2 Default User <ec2-user@ip-172-31-32-54.ec2.internal>
2018-12-21 01:26:45 +0000

    最新、エラー無し

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,10 @@
 class MoviesController < ApplicationController
   def home
-     #@ave = Review.group(:id).average(:hyouka)
+      
+      
+      
+      
+     #@ave = Review.group(:movie_id).average(:hyouka)
      #Reviewテーブルのhyouka（☆の平均を全id分求める）
    
      @movies = Movie.all.each do |movie|
@@ -9,14 +13,16 @@ class MoviesController < ApplicationController
    
      @movies = @movies.sort_by {|movie| movie.average}
      #平均
-   
-     @movies = Movie.includes(:Review) #.order(star: :desc)
+     
+    
+     @reviews = Review.joins(:movie) #.order(star :desc)
      #MovieテーブルとReviewを連結する
     
-     @reviews = Review.page(params[:page]).per(9).order('movie.average DESC')
-     @movies = Movie.page(params[:page]).per(9).order('movie.average DESC')
+     #@reviews = Review.page(params[:page]).per(9).order('star DESC')
+     @movies = Movie.page(params[:page]).per(9).order('star DESC')
      #ページング機能実装に必要な記述、star(星の平均値)順に並べ替え
   end
+  
   
   def index
     #ViewのFormで取得したパラメータをモデルに渡す
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,18 +4,14 @@ class Movie < ApplicationRecord
     validates :synopsis,{length:{maximum:60}}
     validates :grossing,{length:{maximum:20}}
     has_many :reviews
-    
     attr_accessor :average
     
   def average_score
-    self.reviews.sum(:hyouka) / self.reviews.length
+    self.reviews.sum(:hyouka) / 30 #self.reviews.length
   end
   # ↑ attr_accessorで一時的に平均スコアを入れる
     
-  Movie.joins(:review)#Movieテーブルとreviewテーブルを連結
-   #SELECT movies.*FROM movies INNER JOIN reviews ON reviews.id = movies.id AND reviews.hyouka = `Movie`
-    
-  #検索に関する記述
+ #検索に関する記述
   def self.search(search) #self.でクラスメソッドとしている
     if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
       Movie.where(['movie_name LIKE ?', "%#{search}%"])
